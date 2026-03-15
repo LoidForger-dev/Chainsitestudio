9e49c78947cfe8d9b1a3af774561b1f72772a9d4
𝕷𝖔𝖎𝖉 𝕱𝖔𝖗𝖌𝖊𝖗♤ <iammaverick4@gmail.com>
2026-03-15 11:19:51 +0100

    Add configuration for Supabase and wallet addresses

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// Replace with your Supabase URL and anon key
+export const SUPABASE_URL = 'https://ifzykfqbhkfohftdnpkb.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmenlrZnFiaGtmb2hmdGRucGtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NjY0NzcsImV4cCI6MjA4OTE0MjQ3N30.sKLLXn--qDzGNaYjYPVwfpT7iv2fMxIoTTSLyTQz0gE';
+
+// Company wallet address to receive payments
+export const COMPANY_WALLET = 'CRBmFqFnQrYN4ivzM7UcBtMA1S4ApGCYCrMJgukMts8t';
+
+// List of admin wallet addresses
+export const ADMIN_WALLETS = ['0xA4965Eb0Ce0eFbc438DE348c6a0A4F16404C2664', 'bc1qm2cem9gpq4lngfc89xg4aqlcpdf66xdvpquspw'];
